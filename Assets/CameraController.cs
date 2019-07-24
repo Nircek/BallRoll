0a24505c052dcaa01d4a5de7c3bf5156bf4dea08
Nircek <nircek-2103@protonmail.com>
2019-07-24 16:12:38 +0200

    Add CameraController and SpherePlayerController

---
+++ Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform player;
+    public float distance = 15;
+    public float smoothPosition = 1;
+    public float smoothRotation = 1;
+    public float height = 10;
+
+    void Start() {
+    }
+
+    void Update() {
+        Vector3 a = transform.position;
+        Vector3 c = player.position + new Vector3(0, height, 0);
+        float dist = Vector3.Distance(a, c);
+        Vector3 b = ((dist > distance) ? 1 : -1) * (a-Vector3.MoveTowards(a, c, Math.Abs(dist - distance)));
+        Vector3 d = a - b;
+        if (d.y < c.y) d.y = c.y;
+        transform.position = Vector3.Lerp(a, d, smoothPosition*Time.deltaTime);
+        Quaternion lastRotation = transform.rotation;
+        transform.LookAt(player);
+        transform.rotation = Quaternion.Lerp(lastRotation, transform.rotation, smoothRotation*Time.deltaTime);
+    }
+}
---
+++ Assets/SpherePlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePlayerController : MonoBehaviour
+{
+    public float speed = 1;
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow)) direction += Vector3.left;
+        if (Input.GetKey(KeyCode.RightArrow)) direction += Vector3.forward;
+        if (Input.GetKey(KeyCode.DownArrow)) direction += Vector3.right;
+        if (Input.GetKey(KeyCode.LeftArrow)) direction += Vector3.back;
+        transform.GetComponent<Rigidbody>().AddTorque(direction*speed);
+    }
+}
