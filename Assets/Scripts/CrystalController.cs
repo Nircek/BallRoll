afe00110129ab78fff9152d81447744fa114fdce
Nircek <nircek-2103@protonmail.com>
2019-07-26 12:43:10 +0200

    Add the crystals

---
+++ Assets/Scripts/CrystalController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalController : MonoBehaviour
+{
+    int crystalNumber = 0;
+    public GameObject particles;
+
+    void Start()
+    {
+        crystalNumber = Component.FindObjectsOfType<CrystalController>().Length;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "sphere_player") {
+            int remainingNumber = Component.FindObjectsOfType<CrystalController>().Length - 1;
+            int collectedNumber = crystalNumber - remainingNumber;
+            Debug.Log(collectedNumber.ToString() + "/" + crystalNumber.ToString());
+            Instantiate(particles, transform.position, Quaternion.identity);
+            Object.Destroy(gameObject, 0.1f);
+        }
+    }
+}
