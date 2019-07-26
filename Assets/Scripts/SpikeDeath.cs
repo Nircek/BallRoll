dc61e48bce3e6ac51f3c9ce49a9c642bcf3db515
Nircek <nircek-2103@protonmail.com>
2019-07-26 09:15:11 +0200

    Add the spikes moving and the death control

---
+++ Assets/Scripts/SpikeDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeDeath : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "sphere_player")
+            RestartLevel();
+    }
+
+    void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
